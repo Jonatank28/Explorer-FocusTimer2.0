8c9ae13f4522f8172744f2dc214ee4cf9c6c4fec
Jonatank28 <jonatankalmeidakk28@gmail.com>
2022-10-24 09:07:39 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,88 @@
+import sounds from './sounds.js'
+import Sound from './sounds.js'
+
+const buttoPlay = document.querySelector('.buttonPlay')
+const buttonStop = document.querySelector('.buttonStop')
+const buttonSum = document.querySelector('.buttonSum')
+const buttonSubtraction = document.querySelector('.buttonSubtraction')
+const card1 = document.querySelector('.card1')
+const card2 = document.querySelector('.card2')
+const card3 = document.querySelector('.card3')
+const card4 = document.querySelector('.card4')
+let minutes = document.querySelector('.minutes')
+let seconds = document.querySelector('.seconds')
+let timerTimeOut
+let svg 
+
+const sound = Sound()
+
+function resetTimer() {
+    minutes.textContent = minutes.textContent = 2
+}
+
+function sum() {
+    minutes.textContent = Number(minutes.textContent) + 5
+    minutes.textContent = String(minutes.textContent).padStart(2, "0") 
+}
+function subtraction() {
+    minutes.textContent = Number(minutes.textContent) - 5
+    minutes.textContent = String(minutes.textContent).padStart(2, "0") 
+}
+
+function countDown(){
+    sound.relogio.play()
+    timerTimeOut = setTimeout(function(){
+        let newSegunds = Number(seconds.textContent)
+        let newMinutes = Number(minutes.textContent)
+        if(newMinutes <= 0  && newSegunds <= 0) {  
+            sound.despertador.play()
+            return
+        }
+
+        if(newSegunds <= 0 ){
+            minutes.textContent = String(minutes.textContent - 1).padStart(2, "0")
+            newSegunds = 20
+        }
+
+        seconds.textContent = String(newSegunds - 1).padStart(2, "0")
+        countDown()
+    },1000)
+}
+
+buttoPlay.addEventListener('click', () =>{
+    countDown()
+})
+buttonStop.addEventListener('click', () =>{
+    clearTimeout(timerTimeOut)
+})
+buttonSum.addEventListener('click', () =>{
+    sum()
+})
+buttonSubtraction.addEventListener('click', () =>{
+    if(minutes.textContent <= 5){
+        alert(`Número menor de 5`)
+        return
+    }
+    subtraction()
+})
+
+
+card1.addEventListener('click', () =>{
+    console.log(`card1`)
+    sound.lareira.play()
+})
+
+card2.addEventListener('click', () =>{
+    console.log(`card2`)
+})
+
+card3.addEventListener('click', () =>{
+    console.log(`card3`)
+})
+
+card4.addEventListener('click', () =>{
+    console.log(`card4`)
+})
+
+
+
---
+++ sounds.js
@@ -0,0 +1,14 @@
+export default function () {
+
+    const relogio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+    const despertador = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+    const bg = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true")
+    const lareira = new Audio("https://drive.google.com/file/d/1Ip8xBqAUJ-bty51Wz8JBtX_bWXCgA0P2/view?usp=sharing")
+
+    return {
+        relogio,
+        despertador,
+        bg,
+        lareira
+    }
+}
